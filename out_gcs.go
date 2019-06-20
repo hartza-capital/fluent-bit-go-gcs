d9d1136f6c9cc5f7cc2f75fd472dfb0c115cfa5e
Aurélien PERRIER <a.perrier89@gmail.com>
2019-06-20 20:27:17 +0200

    Adding recursive parsing map

--- out_gcs.go
+++ out_gcs.go
@@ -3,7 +3,6 @@ package main
 import (
 	"C"
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -14,6 +13,7 @@ import (
 
 	"github.com/fluent/fluent-bit-go/output"
 	"github.com/google/uuid"
+	jsoniter "github.com/json-iterator/go"
 )
 
 var (
@@ -74,13 +74,13 @@ func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int 
 			timestamp = time.Now()
 		}
 
-		line, err := createJSON(record)
+		line, err := createJSON(timestamp, C.GoString(tag), record)
 		if err != nil {
 			fmt.Printf("error creating message for GCS: %v\n", err)
 			continue
 		}
 
-		objectKey := GenerateObjectKey(values["bucket"], C.GoString(tag), timestamp)
+		objectKey := GenerateObjectKey(values["prefix"], C.GoString(tag), timestamp)
 		if err = gcsClient.Write(values["bucket"], objectKey, bytes.NewReader(line)); err != nil {
 			fmt.Printf("error sending message in GCS: %v\n", err)
 			return output.FLB_RETRY
@@ -107,20 +107,29 @@ func GenerateObjectKey(prefix, tag string, t time.Time) string {
 	return objectKey
 }
 
-func createJSON(record map[interface{}]interface{}) ([]byte, error) {
+func parseMap(mapInterface map[interface{}]interface{}) map[string]interface{} {
 	m := make(map[string]interface{})
 
-	for k, v := range record {
+	for k, v := range mapInterface {
 		switch t := v.(type) {
 		case []byte:
 			// prevent encoding to base64
 			m[k.(string)] = string(t)
+		case map[interface{}]interface{}:
+			m[k.(string)] = parseMap(t)
 		default:
 			m[k.(string)] = v
 		}
 	}
 
-	js, err := json.Marshal(m)
+	return m
+}
+
+func createJSON(timestamp time.Time, tag string, record map[interface{}]interface{}) ([]byte, error) {
+	m := parseMap(record)
+
+	m["timestamp"] = timestamp.UTC().Format(time.RFC3339Nano)
+	js, err := jsoniter.Marshal(m)
 	if err != nil {
 		return []byte("{}"), err
 	}
